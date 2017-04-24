8433e19ed17e3aae9ed62f04e47dc7d9f806b99d
SilvaDiegoUnlam <spookycynicism@gmail.com>
2017-04-24 20:37:43 -0300

    arreglado

--- src/paquete/Matriz.java
+++ src/paquete/Matriz.java
@@ -6,7 +6,44 @@ import java.util.Scanner;
 
 import exceptions.DimensionException;
 
-ion{
+public class Matriz {
+	private double [][] cord; //up to date
+	private int f,c,dim;
+	
+	public Matriz(File file) throws FileNotFoundException{
+		Scanner in = new Scanner(file);
+		f = in.nextInt();
+		c = in.nextInt();
+		dim= f*c;
+		int auxf,auxc;
+		for(int i =0;i<dim;i++){
+			//cord[in.nextInt()][in.nextInt()]=in.nextDouble();
+			auxf=in.nextInt();
+			auxc=in.nextInt();
+			cord[auxf][auxc]=in.nextDouble();
+		}
+	}
+	public Matriz(int f, int c) {
+		this.f=f;
+		this.c=c;
+		dim=f*c;
+		cord = new double [f][c];
+	}
+	public Matriz add(Matriz o) throws DimensionException{
+		if(dim!=o.dim)
+			throw new DimensionException("no esta bien");
+		Matriz aux= new Matriz(f,c);
+		for(int i=0;i<f;i++){
+			for(int j=0;j<c;j++){
+				aux.cord[i][j]=cord[i][j]+o.cord[i][j];
+			}
+		}
+		return aux;
+	}
+	public void add(double n, int f, int c){
+		cord[f][c]=n;
+	}
+	public Matriz substract(Matriz o) throws DimensionException{
 		if(dim!=o.dim)
 			throw new DimensionException("no esta bien");
 		Matriz aux= new Matriz(f,c);
